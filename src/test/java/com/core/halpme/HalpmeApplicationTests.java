d4575e4c8568d7662021e5fb64a6a1d334aa838f
Hyunbin Kim <taque88@naver.com>
2025-05-15 01:11:34 +0900

    [SETTING] 스프링 프로젝트 설정

---
+++ src/test/java/com/core/halpme/HalpmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.halpme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalpmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
