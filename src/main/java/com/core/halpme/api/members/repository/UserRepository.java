62acec33fab334f26c5c5393ad3840de8137acc1
yangwoohyeon <dngus0717@gmail.com>
2025-05-17 23:14:52 +0900

    [FEAT] UserRepository 추가

---
+++ src/main/java/com/core/halpme/api/members/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.core.halpme.api.members.repository;
+
+import com.core.halpme.api.members.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+}
