b18cb9210926e28d2a608b24ef01bc80bd9104b3
Hyunbin Kim <taque88@naver.com>
2025-05-19 17:33:03 +0900

    [FEAT] S3 Service 추가

--- src/main/java/com/core/halpme/api/aws/s3/S3Service.java
+++ src/main/java/com/core/halpme/api/aws/s3/S3Service.java
@@ -1,4 +1,21 @@
 package com.core.halpme.api.aws.s3;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Service
+@RequiredArgsConstructor
 public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${cloud.aws.region.static}")
+    private String bucketName;
+
+    @Value("${cloud.aws.s3.domain}")
+    private String domain;
+
+
 }
