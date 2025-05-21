6d5e2debc4a03b1c713cc501e4debc36bb45a114
Hyunbin Kim <taque88@naver.com>
2025-05-22 00:53:55 +0900

    [FIX] Swagger 설정 수정

--- src/main/java/com/core/halpme/common/config/SwaggerConfig.java
+++ src/main/java/com/core/halpme/common/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.core.halpme.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +11,6 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .addServersItem(new Server().url("/api"))
                 .info(new Info()
                         .title("Halpme API")
                         .description("할프미 프로젝트 Swagger 문서입니다.")
