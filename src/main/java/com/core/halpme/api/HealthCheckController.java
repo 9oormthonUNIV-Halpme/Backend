d0f3f14297b1d8703380e785a9fb0d6944979b3b
Hyunbin Kim <taque88@naver.com>
2025-05-22 00:37:30 +0900

    [FIX] HealthCheck 엔드포인트 수정

--- src/main/java/com/core/halpme/api/HealthCheckController.java
+++ src/main/java/com/core/halpme/api/HealthCheckController.java
@@ -1,12 +1,13 @@
 package com.core.halpme.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/api")
 public class HealthCheckController {
-
 
     @GetMapping("health")
     public String healthCheck() {
