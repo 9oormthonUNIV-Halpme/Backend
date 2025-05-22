f22c40542b0148bd8f6d3acde3c9451a6238023e
yangwoohyeon <dngus0717@gmail.com>
2025-05-23 00:06:20 +0900

    [FIX] 응답값 형식에 맞게 수정

--- src/main/java/com/core/halpme/api/chat/controller/ChatRoomController.java
+++ src/main/java/com/core/halpme/api/chat/controller/ChatRoomController.java
@@ -9,6 +9,8 @@ import com.core.halpme.api.chat.repository.MessageReadStatusRepository;
 import com.core.halpme.api.chat.service.ChatMessageService;
 import com.core.halpme.api.chat.service.ChatRoomService;
 import com.core.halpme.api.members.jwt.SecurityUtil;
+import com.core.halpme.common.response.ApiResponse;
+import com.core.halpme.common.response.SuccessStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -35,33 +37,31 @@ public class ChatRoomController {
     @PostMapping("/personal")
     @Operation(summary = "두 유저 사이의 채팅방 생성", description = "게시글 작성자의 Email 필요")
     @PreAuthorize("isAuthenticated()")
-    public CreateChatRoomResponseDto createPersonalChatRoom(@RequestBody GuestEmailDto request) {
+    public ResponseEntity<ApiResponse<CreateChatRoomResponseDto>> createPersonalChatRoom(@RequestBody GuestEmailDto request) {
         String roomMakerEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-        return chatRoomService.createChatRoomForPersonal(roomMakerEmail, request.getGuestEmail());
+        CreateChatRoomResponseDto response = chatRoomService.createChatRoomForPersonal(roomMakerEmail, request.getGuestEmail());
+        return ApiResponse.success(SuccessStatus.CHAT_ROOM_CREATE_SUCCESS, response);
     }
+
 
 
     @GetMapping("/messages")
     @Operation(summary = "채팅방 채팅 기록 반환", description = "RoomId 필요")
-    public ResponseEntity<List<ChatMessageDto>> getMessagesByRoomId(
-            @RequestParam String roomId
-    ) {
+    public ResponseEntity<ApiResponse<List<ChatMessageDto>>> getMessagesByRoomId(@RequestParam String roomId) {
         List<ChatMessage> messages = chatMessageService.getMessagesByRoomId(roomId);
         List<ChatMessageDto> response = messages.stream()
                 .map(ChatMessageDto::fromEntity)
                 .collect(Collectors.toList());
-        return ResponseEntity.ok(response);
+        return ApiResponse.success(SuccessStatus.CHAT_MESSAGES_GET_SUCCESS, response);
     }
 
 
     @GetMapping("/rooms")
     @Operation(summary = "사용자의 채팅방 목록 보기", description = "인증 토큰 필요")
-    public ResponseEntity<List<ChatRoomDto>> getChatRooms() {
-        // 현재 로그인한 사용자 이메일 꺼내기
+    public ResponseEntity<ApiResponse<List<ChatRoomDto>>> getChatRooms() {
         String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-
         List<ChatRoomDto> rooms = chatRoomService.getChatRoomsForUser(userEmail);
-        return ResponseEntity.ok(rooms);
+        return ApiResponse.success(SuccessStatus.CHATROOM_LIST_SUCCESS, rooms);
     }
 
 }
