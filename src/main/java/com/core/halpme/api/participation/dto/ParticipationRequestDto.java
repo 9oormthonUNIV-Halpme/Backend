2504deb24a4dce5fb70379e39d1a9dc779464ed1
Hyunbin Kim <taque88@naver.com>
2025-05-25 04:39:18 +0900

    [REFACTOR] myPage와 participation 디렉토리 삭제 (충돌 방지)

--- src/main/java/com/core/halpme/api/participation/dto/ParticipationRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.core.halpme.api.participation.dto;
-
-import lombok.Getter;
-
-@Getter
-public class ParticipationRequestDto {
-    //신청할 게시글 ID
-    private Long postId;
-}
--- src/main/java/com/core/halpme/api/post/entity/PostMemberType.java
+++
@@ -1,9 +0,0 @@
-package com.core.halpme.api.post.entity;
-
-import lombok.Getter;
-
-@Getter
-public enum PostMemberType {
-    USER,
-    REQUESTER
-}
