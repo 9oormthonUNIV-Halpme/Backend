ab4742c6c14792e192e38fd3e36c2960ce3978cf
Hyunbin Kim <taque88@naver.com>
2025-05-26 22:21:17 +0900

    [REFACTOR] CORS 설정 리팩토링

--- src/main/java/com/core/halpme/common/config/WebSocketConfig.java
+++ src/main/java/com/core/halpme/common/config/WebSocketConfig.java
@@ -1,17 +1,11 @@
 package com.core.halpme.common.config;
 
 import com.core.halpme.api.chat.auth.StompHandler;
-import com.core.halpme.api.chat.auth.StompPrincipal;
 import com.core.halpme.api.members.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
