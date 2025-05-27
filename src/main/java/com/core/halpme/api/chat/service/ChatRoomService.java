446ef7b8bf7216dc9ed25618557dcdacd48e55b9
yangwoohyeon <dngus0717@gmail.com>
2025-05-27 23:12:44 +0900

    [REFACT] 채팅방 상대방 닉네임 조회 기능 추가

--- src/main/java/com/core/halpme/api/chat/service/ChatRoomService.java
+++ src/main/java/com/core/halpme/api/chat/service/ChatRoomService.java
@@ -9,4 +9,5 @@ import java.util.List;
 public interface ChatRoomService {
     CreateChatRoomResponseDto createChatRoomForPersonal(String roomMakerEmail, String guestEmail);
     List<ChatRoomDto> getChatRoomsForUser(String email);
+    String getOpponentNickname(String roomId, String currentUserEmail);
 }
